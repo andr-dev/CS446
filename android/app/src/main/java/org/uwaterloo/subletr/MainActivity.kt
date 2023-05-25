87bc5b62f54d21a6b4b1242a0cb935262e03a1fd
Min-Kyu Lee <63979360+leeminkyu16@users.noreply.github.com>
2023-05-25 19:01:53 -0400

    Create Home Page View

--- android/app/src/main/java/org/uwaterloo/subletr/MainActivity.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/MainActivity.kt
@@ -3,15 +3,31 @@ package org.uwaterloo.subletr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.uwaterloo.subletr.scaffold.MainScaffoldView
+import org.uwaterloo.subletr.theme.SubletrTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		WindowCompat.setDecorFitsSystemWindows(window, false)
+
 		setContent {
-			MainScaffoldView()
+			SubletrTheme {
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					MainScaffoldView()
+				}
+			}
 		}
 	}
 }
