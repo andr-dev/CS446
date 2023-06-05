966fed0e68916d516844d91cdacf758ddef0b553
Min-Kyu Lee <63979360+leeminkyu16@users.noreply.github.com>
2023-06-05 14:31:18 -0400

    Create Color System for Dark Mode

--- android/app/src/main/java/org/uwaterloo/subletr/theme/SubletrColors.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/theme/SubletrColors.kt
@@ -2,23 +2,62 @@
 
 package org.uwaterloo.subletr.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val Purple900 = Color(0xFF4A148C)
+val subletrPink
+@Composable
+	get() = if (isSystemInDarkTheme()) {
+		subletrPinkDark
+	}
+	else {
+		subletrPinkLight
+	}
 
+private val subletrPinkLight = Color(0xFFF4717F)
+private val subletrPinkDark = Color(0xFFF4717F)
+
+val unselectedGray
+	@Composable
+	get() = if (isSystemInDarkTheme()) {
+		unselectedGrayDark
+	}
+	else {
+		unselectedGrayLight
+	}
+val unselectedGrayLight = Color(0xFF808080)
+val unselectedGrayDark = Color(0xFF808080)
+
+val secondaryTextColor
+	@Composable
+	get() = if (isSystemInDarkTheme()) {
+		secondaryTextColorDark
+	}
+	else {
+		secondaryTextColorLight
+	}
+val secondaryTextColorLight = Color((0xFF808080))
+val secondaryTextColorDark = Color((0xFF808080))
+
+val buttonBackgroundColor
+	@Composable
+	get() = if (isSystemInDarkTheme()) {
+		buttonBackgroundColorDark
+	}
+	else {
+		buttonBackgroundColorLight
+	}
+val buttonBackgroundColorLight = Color((0xFFD9D9D9))
+val buttonBackgroundColorDark = Color((0xFFD9D9D9))
+
+val Purple900 = Color(0xFF4A148C)
 val Purple500 = Color(0xFF9C27B0)
 
-val subletrPink = Color(0xFFF4717F)
-
-val unselectedGray = Color(0xFF808080)
-
-val secondaryTextColor = Color((0xFF808080))
-val buttonBackgroundColor = Color((0xFFD9D9D9))
-
 internal val SubletrDarkColorScheme = darkColorScheme(
-	primary = subletrPink,
+	primary = subletrPinkDark,
 	tertiary = Purple900,
 	background = Color(0xFFFFFBFE),
 	surface = Color(0xFFFFFBFE),
@@ -30,7 +69,7 @@ internal val SubletrDarkColorScheme = darkColorScheme(
 )
 
 internal val SubletrLightColorScheme = lightColorScheme(
-	primary = subletrPink,
+	primary = subletrPinkLight,
 	tertiary = Purple500,
 	background = Color(0xFFFFFBFE),
 	surface = Color(0xFFFFFBFE),
