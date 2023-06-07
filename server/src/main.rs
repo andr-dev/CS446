a7b86124d83bffdd899bde68a8386711dfe18552
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-06-07 18:51:40 -0400

    Add swagger UI + server static serve

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
 extern crate proc_macro;
 
 mod api;
+use api::rocket;
 
 mod cors;
 use cors::CORS;
@@ -11,13 +12,14 @@ mod error;
 mod state;
 use state::AppState;
 
-use api::rocket;
+use rocket::fs::FileServer;
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app_state = AppState::new().await?;
 
     let _ = rocket()
+        .mount("/", FileServer::from("../docs"))
         .manage::<AppState>(app_state)
         .attach(CORS)
         .launch()
