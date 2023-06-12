8718fe3ebf4e314fc9d81fdf88c7dde0448e772c
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-06-12 14:42:41 -0400

    Build listing protos

--- android/app/src/main/java/org/uwaterloo/subletr/api/apis/DefaultApi.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/api/apis/DefaultApi.kt
@@ -21,6 +21,8 @@ import okhttp3.HttpUrl
 
 import org.uwaterloo.subletr.api.models.CreateUserRequest
 import org.uwaterloo.subletr.api.models.CreateUserResponse
+import org.uwaterloo.subletr.api.models.GetListingDetailsResponse
+import org.uwaterloo.subletr.api.models.GetListingsResponse
 import org.uwaterloo.subletr.api.models.UserLoginRequest
 import org.uwaterloo.subletr.api.models.UserLoginResponse
 
@@ -125,6 +127,80 @@ class DefaultApi(basePath: kotlin.String = defaultBasePath, client: OkHttpClient
     /**
      * 
      * 
+     * @param listingId 
+     * @return GetListingDetailsResponse
+     * @throws IllegalStateException If the request is not correctly configured
+     * @throws IOException Rethrows the OkHttp execute method exception
+     * @throws UnsupportedOperationException If the API returns an informational or redirection response
+     * @throws ClientException If the API returns a client error response
+     * @throws ServerException If the API returns a server error response
+     */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IllegalStateException::class, IOException::class, UnsupportedOperationException::class, ClientException::class, ServerException::class)
+    suspend fun details(listingId: kotlin.String) : GetListingDetailsResponse = withContext(Dispatchers.IO) {
+        val localVarResponse = detailsWithHttpInfo(listingId = listingId)
+
+        return@withContext when (localVarResponse.responseType) {
+            ResponseType.Success -> (localVarResponse as Success<*>).data as GetListingDetailsResponse
+            ResponseType.Informational -> throw UnsupportedOperationException("Client does not support Informational responses.")
+            ResponseType.Redirection -> throw UnsupportedOperationException("Client does not support Redirection responses.")
+            ResponseType.ClientError -> {
+                val localVarError = localVarResponse as ClientError<*>
+                throw ClientException("Client error : ${localVarError.statusCode} ${localVarError.message.orEmpty()}", localVarError.statusCode, localVarResponse)
+            }
+            ResponseType.ServerError -> {
+                val localVarError = localVarResponse as ServerError<*>
+                throw ServerException("Server error : ${localVarError.statusCode} ${localVarError.message.orEmpty()}", localVarError.statusCode, localVarResponse)
+            }
+        }
+    }
+
+    /**
+     * 
+     * 
+     * @param listingId 
+     * @return ApiResponse<GetListingDetailsResponse?>
+     * @throws IllegalStateException If the request is not correctly configured
+     * @throws IOException Rethrows the OkHttp execute method exception
+     */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IllegalStateException::class, IOException::class)
+    suspend fun detailsWithHttpInfo(listingId: kotlin.String) : ApiResponse<GetListingDetailsResponse?> = withContext(Dispatchers.IO) {
+        val localVariableConfig = detailsRequestConfig(listingId = listingId)
+
+        return@withContext request<Unit, GetListingDetailsResponse>(
+            localVariableConfig
+        )
+    }
+
+    /**
+     * To obtain the request config of the operation details
+     *
+     * @param listingId 
+     * @return RequestConfig
+     */
+    fun detailsRequestConfig(listingId: kotlin.String) : RequestConfig<Unit> {
+        val localVariableBody = null
+        val localVariableQuery: MultiValueMap = mutableMapOf<kotlin.String, kotlin.collections.List<kotlin.String>>()
+            .apply {
+                put("listing_id", listOf(listingId.toString()))
+            }
+        val localVariableHeaders: MutableMap<String, String> = mutableMapOf()
+        localVariableHeaders["Accept"] = "application/json"
+
+        return RequestConfig(
+            method = RequestMethod.GET,
+            path = "/listings/details",
+            query = localVariableQuery,
+            headers = localVariableHeaders,
+            requiresAuthentication = false,
+            body = localVariableBody
+        )
+    }
+
+    /**
+     * 
+     * 
      * @return kotlin.String
      * @throws IllegalStateException If the request is not correctly configured
      * @throws IOException Rethrows the OkHttp execute method exception
@@ -183,6 +259,100 @@ class DefaultApi(basePath: kotlin.String = defaultBasePath, client: OkHttpClient
         return RequestConfig(
             method = RequestMethod.GET,
             path = "/user/email",
+            query = localVariableQuery,
+            headers = localVariableHeaders,
+            requiresAuthentication = false,
+            body = localVariableBody
+        )
+    }
+
+    /**
+     * 
+     * 
+     * @param priceMin  (optional)
+     * @param priceMax  (optional)
+     * @param roomsMin  (optional)
+     * @param roomsMax  (optional)
+     * @return GetListingsResponse
+     * @throws IllegalStateException If the request is not correctly configured
+     * @throws IOException Rethrows the OkHttp execute method exception
+     * @throws UnsupportedOperationException If the API returns an informational or redirection response
+     * @throws ClientException If the API returns a client error response
+     * @throws ServerException If the API returns a server error response
+     */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IllegalStateException::class, IOException::class, UnsupportedOperationException::class, ClientException::class, ServerException::class)
+    suspend fun list(priceMin: kotlin.Int? = null, priceMax: kotlin.Int? = null, roomsMin: kotlin.Int? = null, roomsMax: kotlin.Int? = null) : GetListingsResponse = withContext(Dispatchers.IO) {
+        val localVarResponse = listWithHttpInfo(priceMin = priceMin, priceMax = priceMax, roomsMin = roomsMin, roomsMax = roomsMax)
+
+        return@withContext when (localVarResponse.responseType) {
+            ResponseType.Success -> (localVarResponse as Success<*>).data as GetListingsResponse
+            ResponseType.Informational -> throw UnsupportedOperationException("Client does not support Informational responses.")
+            ResponseType.Redirection -> throw UnsupportedOperationException("Client does not support Redirection responses.")
+            ResponseType.ClientError -> {
+                val localVarError = localVarResponse as ClientError<*>
+                throw ClientException("Client error : ${localVarError.statusCode} ${localVarError.message.orEmpty()}", localVarError.statusCode, localVarResponse)
+            }
+            ResponseType.ServerError -> {
+                val localVarError = localVarResponse as ServerError<*>
+                throw ServerException("Server error : ${localVarError.statusCode} ${localVarError.message.orEmpty()}", localVarError.statusCode, localVarResponse)
+            }
+        }
+    }
+
+    /**
+     * 
+     * 
+     * @param priceMin  (optional)
+     * @param priceMax  (optional)
+     * @param roomsMin  (optional)
+     * @param roomsMax  (optional)
+     * @return ApiResponse<GetListingsResponse?>
+     * @throws IllegalStateException If the request is not correctly configured
+     * @throws IOException Rethrows the OkHttp execute method exception
+     */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IllegalStateException::class, IOException::class)
+    suspend fun listWithHttpInfo(priceMin: kotlin.Int?, priceMax: kotlin.Int?, roomsMin: kotlin.Int?, roomsMax: kotlin.Int?) : ApiResponse<GetListingsResponse?> = withContext(Dispatchers.IO) {
+        val localVariableConfig = listRequestConfig(priceMin = priceMin, priceMax = priceMax, roomsMin = roomsMin, roomsMax = roomsMax)
+
+        return@withContext request<Unit, GetListingsResponse>(
+            localVariableConfig
+        )
+    }
+
+    /**
+     * To obtain the request config of the operation list
+     *
+     * @param priceMin  (optional)
+     * @param priceMax  (optional)
+     * @param roomsMin  (optional)
+     * @param roomsMax  (optional)
+     * @return RequestConfig
+     */
+    fun listRequestConfig(priceMin: kotlin.Int?, priceMax: kotlin.Int?, roomsMin: kotlin.Int?, roomsMax: kotlin.Int?) : RequestConfig<Unit> {
+        val localVariableBody = null
+        val localVariableQuery: MultiValueMap = mutableMapOf<kotlin.String, kotlin.collections.List<kotlin.String>>()
+            .apply {
+                if (priceMin != null) {
+                    put("price_min", listOf(priceMin.toString()))
+                }
+                if (priceMax != null) {
+                    put("price_max", listOf(priceMax.toString()))
+                }
+                if (roomsMin != null) {
+                    put("rooms_min", listOf(roomsMin.toString()))
+                }
+                if (roomsMax != null) {
+                    put("rooms_max", listOf(roomsMax.toString()))
+                }
+            }
+        val localVariableHeaders: MutableMap<String, String> = mutableMapOf()
+        localVariableHeaders["Accept"] = "application/json"
+
+        return RequestConfig(
+            method = RequestMethod.GET,
+            path = "/listings/list",
             query = localVariableQuery,
             headers = localVariableHeaders,
             requiresAuthentication = false,
