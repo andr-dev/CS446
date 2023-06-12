ad81c20ac272fb0d43f4043123e42047ee694b92
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-06-12 13:59:58 -0400

    Add openapi kotlin client singleton

--- android/app/src/main/java/org/uwaterloo/subletr/di/SingletonModule.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/di/SingletonModule.kt
@@ -1,9 +1,18 @@
 package org.uwaterloo.subletr.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.uwaterloo.subletr.api.apis.DefaultApi
 
 @Module
 @InstallIn(SingletonComponent::class)
-object SingletonModule
+object SingletonModule {
+	val api = DefaultApi("http://207.246.122.186/api")
+
+	@Provides
+	fun provideApiClient(): DefaultApi {
+		return api;
+	}
+}
