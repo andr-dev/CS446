2f23bc9c0fde5c04dbee61cef0534f4b129a1316
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-06-14 14:44:43 -0400

    Add listings db and update endpoints

--- server/src/api/model/listing.rs
+++ server/src/api/model/listing.rs
@@ -5,6 +5,8 @@ use rocket::FromForm;
 use rocket_okapi::JsonSchema;
 use serde::Serialize;
 
+use crate::{db::model::listings::Listing, error::ServiceError};
+
 #[derive(JsonSchema, FromForm, Clone, PartialEq)]
 pub struct GetListingsRequest {
     pub price_min: Option<u16>,
@@ -21,7 +23,7 @@ pub struct GetListingsResponse {
 
 #[derive(JsonSchema, Serialize, Clone, PartialEq)]
 pub struct ListingSummary {
-    pub listing_id: String,
+    pub listing_id: i64,
     pub address: String,
     pub price: u16,
     pub rooms: u16,
@@ -30,16 +32,44 @@ pub struct ListingSummary {
     pub residence_type: ResidenceType,
 }
 
+impl TryFrom<Listing> for ListingSummary {
+    type Error = ServiceError;
+
+    fn try_from(l: Listing) -> Result<Self, Self::Error> {
+        Ok(ListingSummary {
+            listing_id: l.listing_id,
+            address: format!(
+                "{}, {} {}, {}",
+                l.address_line, l.address_city, l.address_postalcode, l.address_country
+            ),
+            price: l.price.try_into().map_err(|_| ServiceError::InternalError)?,
+            rooms: l.rooms.try_into().map_err(|_| ServiceError::InternalError)?,
+            lease_start: l
+                .lease_start
+                .and_local_timezone(Utc)
+                .single()
+                .ok_or(ServiceError::InternalError)?,
+            lease_end: l
+                .lease_end
+                .and_local_timezone(Utc)
+                .single()
+                .ok_or(ServiceError::InternalError)?,
+            residence_type: ResidenceType::Other,
+        })
+    }
+}
+
 #[derive(JsonSchema, Serialize, Clone, PartialEq)]
 pub enum ResidenceType {
     Apartment,
     House,
     TownHouse,
+    Other,
 }
 
 #[derive(JsonSchema, FromForm, Clone, PartialEq)]
 pub struct GetListingDetailsRequest {
-    pub listing_id: String,
+    pub listing_id: i64,
 }
 
 #[derive(JsonSchema, Serialize, Clone, PartialEq)]
@@ -58,5 +88,25 @@ pub struct ListingDetails {
     pub description: String,
     pub img_ids: Vec<String>,
     pub residence_type: ResidenceType,
-    pub owner_id: String,
+    pub owner_user_id: i64,
+}
+
+impl TryFrom<Listing> for ListingDetails {
+    type Error = ServiceError;
+
+    fn try_from(l: Listing) -> Result<Self, Self::Error> {
+        let ls = ListingSummary::try_from(l.clone())?;
+
+        Ok(ListingDetails {
+            address: ls.address,
+            price: ls.price,
+            rooms: ls.rooms,
+            lease_start: ls.lease_start,
+            lease_end: ls.lease_end,
+            description: l.listing_description,
+            img_ids: vec![],
+            residence_type: ResidenceType::Other,
+            owner_user_id: l.owner_user_id,
+        })
+    }
 }
--- server/src/db/schema.rs
+++ server/src/db/schema.rs
@@ -1,6 +1,23 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    listings (listing_id) {
+        listing_id -> BigInt,
+        address_line -> Text,
+        address_city -> Text,
+        address_postalcode -> Text,
+        address_country -> Text,
+        price -> BigInt,
+        rooms -> BigInt,
+        lease_start -> Timestamp,
+        lease_end -> Timestamp,
+        listing_description -> Text,
+        residence_type -> Text,
+        owner_user_id -> BigInt,
+    }
+}
+
+diesel::table! {
     users (user_id) {
         user_id -> BigInt,
         first_name -> Text,
@@ -10,3 +27,10 @@ diesel::table! {
         gender -> Text,
     }
 }
+
+diesel::joinable!(listings -> users (owner_user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    listings,
+    users,
+);
