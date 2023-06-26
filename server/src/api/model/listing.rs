e275008abd503ebea21f8e6963e825fbb361e0b8
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-06-26 17:54:35 -0400

    Add img_ids to listing details

--- server/src/api/model/listing.rs
+++ server/src/api/model/listing.rs
@@ -32,13 +32,12 @@ pub struct ListingSummary {
     pub rooms: u16,
     pub lease_start: DateTime<Utc>,
     pub lease_end: DateTime<Utc>,
+    pub img_ids: Vec<String>,
     pub residence_type: ResidenceType,
 }
 
-impl TryFrom<Listing> for ListingSummary {
-    type Error = ServiceError;
-
-    fn try_from(l: Listing) -> Result<Self, Self::Error> {
+impl ListingSummary {
+    pub fn try_from_db(l: Listing, img_ids: Vec<String>) -> Result<Self, ServiceError> {
         Ok(ListingSummary {
             listing_id: l.listing_id,
             address: format!(
@@ -57,6 +56,7 @@ impl TryFrom<Listing> for ListingSummary {
                 .and_local_timezone(Utc)
                 .single()
                 .ok_or(ServiceError::InternalError)?,
+            img_ids,
             residence_type: ResidenceType::Other,
         })
     }
@@ -97,7 +97,11 @@ pub struct ListingDetails {
 
 impl ListingDetails {
     pub fn try_from_db(l: Listing, img_ids: Vec<String>) -> Result<Self, ServiceError> {
-        let ls = ListingSummary::try_from(l.clone())?;
+        let description = l.listing_description.to_owned();
+        let residence_type = ResidenceType::from_string(&l.residence_type).ok_or(ServiceError::InternalError)?;
+        let owner_user_id = l.owner_user_id;
+
+        let ls = ListingSummary::try_from_db(l, img_ids)?;
 
         Ok(ListingDetails {
             address: ls.address,
@@ -105,10 +109,10 @@ impl ListingDetails {
             rooms: ls.rooms,
             lease_start: ls.lease_start,
             lease_end: ls.lease_end,
-            description: l.listing_description,
-            img_ids,
-            residence_type: ResidenceType::from_string(&l.residence_type).ok_or(ServiceError::InternalError)?,
-            owner_user_id: l.owner_user_id,
+            description,
+            img_ids: ls.img_ids,
+            residence_type,
+            owner_user_id,
         })
     }
 }
