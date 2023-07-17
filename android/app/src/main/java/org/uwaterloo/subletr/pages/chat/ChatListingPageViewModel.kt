934b9d5b5c6618e791153fdc6033c3296344bbc3
Alex Lin <linpeixi@gmail.com>
2023-07-17 10:10:33 -0400

    added ui state for chat listing

--- android/app/src/main/java/org/uwaterloo/subletr/pages/chat/ChatListingPageViewModel.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/pages/chat/ChatListingPageViewModel.kt
@@ -1,26 +1,24 @@
 package org.uwaterloo.subletr.pages.chat
 
-import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import org.uwaterloo.subletr.services.INavigationService
 import javax.inject.Inject
+import org.uwaterloo.subletr.infrastructure.SubletrViewModel
 
 @HiltViewModel
 class ChatListingPageViewModel @Inject constructor(
 	navigationService: INavigationService,
-) : ViewModel() {
-	val uiStateStream: BehaviorSubject<ChatListingPageUiState> = BehaviorSubject.createDefault(
+) : SubletrViewModel<ChatListingPageUiState>() {
+	override val uiStateStream: BehaviorSubject<ChatListingPageUiState> = BehaviorSubject.createDefault(
 		ChatListingPageUiState.Loaded(
 			contacts = emptyList()
 		)
 	)
 }
 
-class Contact(
-	name: String = "Alex Lin",
-	msg: List<String> = List(1){ "this is a test dialog" }
-) {
-	val name: String = name
-	val msg: List<String> = msg
-}
+data class Contact(
+	var name: String,
+	var msg: List<String>,
+	var unread : Boolean,
+)
