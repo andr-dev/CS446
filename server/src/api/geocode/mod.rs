cbe8ffbe93545fdff476a37efcc50ece6a2ec471
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2023-07-17 16:01:00 -0400

    Add location fetch to listings_create

--- server/src/api/geocode/mod.rs
+++ server/src/api/geocode/mod.rs
@@ -2,6 +2,7 @@ use geo_types::Point;
 use okapi::openapi3::OpenApi;
 use rocket::{get, serde::json::Json, Route, State};
 use rocket_okapi::{openapi, openapi_get_routes_spec};
+use tokio::sync::RwLockWriteGuard;
 
 use super::{
     model::geocode::{ReverseGeocodeRequest, ReverseGeocodeResponse},
@@ -9,9 +10,25 @@ use super::{
 };
 use crate::{
     error::{ServiceError, ServiceResult},
-    geocode::{Forward, GeocodingError},
+    geocode::{opencage::Opencage, Forward, GeocodingError},
     state::AppState,
 };
+
+pub async fn address_reverse(
+    opencage: RwLockWriteGuard<'_, Opencage<'_>>,
+    address: String,
+) -> Result<ReverseGeocodeResponse, ServiceError> {
+    let response: Vec<Point> = opencage.forward(&address).await?;
+
+    if let Some(point) = response.first() {
+        Ok(ReverseGeocodeResponse {
+            latitude: point.0.y as f32,
+            longitude: point.0.x as f32,
+        })
+    } else {
+        Err(ServiceError::GeocodingError(GeocodingError::Forward))
+    }
+}
 
 #[openapi(tag = "Geocode")]
 #[get("/reverse?<geocode_request..>")]
@@ -21,23 +38,14 @@ async fn reverse_geocode(
 ) -> ServiceResult<ReverseGeocodeResponse> {
     let opencage = state.opencage.write().await;
 
-    let response: Vec<Point> = opencage
-        .forward(&format_address(
-            geocode_request.address_line,
-            geocode_request.address_city,
-            geocode_request.address_postalcode,
-            geocode_request.address_country,
-        ))
-        .await?;
+    let address = format_address(
+        geocode_request.address_line,
+        geocode_request.address_city,
+        geocode_request.address_postalcode,
+        geocode_request.address_country,
+    );
 
-    if let Some(point) = response.first() {
-        Ok(Json(ReverseGeocodeResponse {
-            latitude: point.0.y as f32,
-            longitude: point.0.x as f32,
-        }))
-    } else {
-        Err(ServiceError::GeocodingError(GeocodingError::Forward))
-    }
+    address_reverse(opencage, address).await.map(|resp| Json(resp))
 }
 
 pub fn routes() -> (Vec<Route>, OpenApi) {
