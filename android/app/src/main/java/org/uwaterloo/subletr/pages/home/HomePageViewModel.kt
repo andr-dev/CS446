bcc4a48bc4c10a9791abc602ce665cffd5e5b660
Henry Hu <huhenry50@gmail.com>
2023-07-17 17:45:15 -0400

    use view model scope for location requests

--- android/app/src/main/java/org/uwaterloo/subletr/pages/home/HomePageViewModel.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/pages/home/HomePageViewModel.kt
@@ -1,11 +1,14 @@
 package org.uwaterloo.subletr.pages.home
 
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Observable
+import kotlinx.coroutines.launch
 import org.uwaterloo.subletr.infrastructure.SubletrViewModel
 import org.uwaterloo.subletr.pages.home.list.HomeListChildViewModel
 import org.uwaterloo.subletr.pages.home.map.HomeMapChildViewModel
+import org.uwaterloo.subletr.services.ILocationService
 import org.uwaterloo.subletr.services.INavigationService
 import javax.inject.Inject
 
@@ -14,6 +17,7 @@ class HomePageViewModel @Inject constructor(
 	private val navigationService: INavigationService,
 	val homeListChildViewModel: HomeListChildViewModel,
 	val homeMapChildViewModel: HomeMapChildViewModel,
+	private val locationService: ILocationService,
 ) : SubletrViewModel<HomePageUiState>(
 	homeListChildViewModel,
 	homeMapChildViewModel,
@@ -22,5 +26,12 @@ class HomePageViewModel @Inject constructor(
 
 	override val uiStateStream: Observable<HomePageUiState> = homeListChildViewModel.uiStateStream.map {
 		it
+	}
+
+	init {
+		viewModelScope.launch {
+			val loc = locationService.getLocation()
+			println(loc.toString())
+		}
 	}
 }
