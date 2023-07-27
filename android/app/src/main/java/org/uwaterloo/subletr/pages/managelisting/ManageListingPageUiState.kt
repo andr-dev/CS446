bb0f3fd9c1d46c4b2dd9fc70bf273b2e94513e5b
AhbedShawarma <qaztheman64@gmail.com>
2023-07-27 17:39:25 -0400

    remove extra classes in manage listing UI state

--- android/app/src/main/java/org/uwaterloo/subletr/pages/managelisting/ManageListingPageUiState.kt
+++ android/app/src/main/java/org/uwaterloo/subletr/pages/managelisting/ManageListingPageUiState.kt
@@ -10,33 +10,6 @@ import org.uwaterloo.subletr.enums.ListingForGenderOption
 sealed interface ManageListingPageUiState {
 	object Loading : ManageListingPageUiState
 
-	data class AddressModel (
-		val fullAddress: String,
-		val addressLine: String,
-		val addressCity: String,
-		val addressPostalCode: String,
-		val addressCountry: String
-	)
-
-	data class test(
-		val address: String,
-		val description: String,
-		val price: Int,
-		val numBedrooms: Int,
-		val totalNumBedrooms: Int,
-		val numBathrooms: Int,
-		val bathroomsEnsuite: EnsuiteBathroomOption,
-		val totalNumBathrooms: Int,
-		val gender: ListingForGenderOption,
-		val startDate: String,
-		val endDate: String,
-		val startDateDisplayText: String,
-		val endDateDisplayText: String,
-		val housingType: HousingType,
-		val imagesBitmap: MutableList<Bitmap?>,
-		val images: List<String>,
-	)
-
 	data class Loaded(
 		val address: String,
 		val images: List<Bitmap>,
